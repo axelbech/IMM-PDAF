1b154a0fa3ca6e8234ebfe35b76e86972ecca2b1
axeltb <axeltb@stud.ntnu.no>
2020-10-11 12:30:11 +0200

    got a working joyride position plot with a IMM filter, need more filters and more tuning and more plots

--- run_joyride.py
+++ run_joyride.py
@@ -13,6 +13,13 @@ import ekf
 import imm
 import pda
 
+# %% custom imports
+from typing import List
+
+from gaussparams import GaussParams
+from mixturedata import MixtureParameters
+import estimationstatistics as estats
+
 # %% plot config check and style setup
 
 
@@ -69,12 +76,10 @@ except Exception as e:
 filename_to_load = "data_joyride.mat"
 loaded_data = scipy.io.loadmat(filename_to_load)
 K = loaded_data["K"].item()
-Ts = loaded_data["Ts"].squeeze()
+Ts = loaded_data["Ts"].squeeze() # different from run imm PDA
+Ts = np.append(Ts, 2.5)
 Xgt = loaded_data["Xgt"].T
 Z = [zk.T for zk in loaded_data["Z"].ravel()]
-
-
-# plot measurements close to the trajectory
 
 # plot measurements close to the trajectory
 fig1, ax1 = plt.subplots(num=1, clear=True)
@@ -90,25 +95,154 @@ ax1.plot(*Xgt.T[:2], color="C0", linewidth=1.5)
 ax1.set_title("True trajectory and the nearby measurements")
 plt.show(block=False)
 
-# %% play measurement movie. Remember that you can cross out the window
-play_movie = True
-play_slice = slice(0, K)
-if play_movie:
-    if "inline" in matplotlib.get_backend():
-        print("the movie might not play with inline plots")
-    fig2, ax2 = plt.subplots(num=2, clear=True)
-    sh = ax2.scatter(np.nan, np.nan)
-    th = ax2.set_title(f"measurements at step 0")
-    mins = np.vstack(Z).min(axis=0)
-    maxes = np.vstack(Z).max(axis=0)
-    ax2.axis([mins[0], maxes[0], mins[1], maxes[1]])
-    plotpause = 0.1
-    # sets a pause in between time steps if it goes to fast
-    for k, Zk in enumerate(Z[play_slice]):
-        sh.set_offsets(Zk)
-        th.set_text(f"measurements at step {k}")
-        fig2.canvas.draw_idle()
-        plt.show(block=False)
-        plt.pause(plotpause)
+# =============================================================================
+# # %% play measurement movie. Remember that you can cross out the window
+# play_movie = True
+# play_slice = slice(0, K)
+# if play_movie:
+#     if "inline" in matplotlib.get_backend():
+#         print("the movie might not play with inline plots")
+#     fig2, ax2 = plt.subplots(num=2, clear=True)
+#     sh = ax2.scatter(np.nan, np.nan)
+#     th = ax2.set_title(f"measurements at step 0")
+#     mins = np.vstack(Z).min(axis=0)
+#     maxes = np.vstack(Z).max(axis=0)
+#     ax2.axis([mins[0], maxes[0], mins[1], maxes[1]])
+#     plotpause = 0.1
+#     # sets a pause in between time steps if it goes to fast
+#     for k, Zk in enumerate(Z[play_slice]):
+#         sh.set_offsets(Zk)
+#         th.set_text(f"measurements at step {k}")
+#         fig2.canvas.draw_idle()
+#         plt.show(block=False)
+#         plt.pause(plotpause)
+# =============================================================================
 
 # %% setup and track
+
+# sensor
+sigma_z = 2.3 # 10
+clutter_intensity = 2 / (4000*4000) # 1e-2
+PD = 0.85 # 0.8
+gate_size = 4
+
+# dynamic models
+sigma_a_CV = 6
+sigma_a_CT = 6
+sigma_omega = 0.3
+
+
+# markov chain
+PI11 = 0.9
+PI22 = 0.9
+
+p10 = 0.9  # initvalue for mode probabilities
+
+PI = np.array([[PI11, (1 - PI11)], [(1 - PI22), PI22]])
+assert np.allclose(np.sum(PI, axis=1), 1), "rows of PI must sum to 1"
+
+mean_init = np.array([7100, 3620, 0, 0, 0])
+cov_init = np.diag([40, 40, 10, 10, 0.1]) ** 2 # np.diag([1000, 1000, 30, 30, 0.1]) ** 2  # THIS WILL NOT BE GOOD
+mode_probabilities_init = np.array([p10, (1 - p10)])
+mode_states_init = GaussParams(mean_init, cov_init)
+init_imm_state = MixtureParameters(mode_probabilities_init, [mode_states_init] * 2)
+
+assert np.allclose(
+    np.sum(mode_probabilities_init), 1
+), "initial mode probabilities must sum to 1"
+
+# make model
+measurement_model = measurementmodels.CartesianPosition(sigma_z, state_dim=5)
+dynamic_models: List[dynamicmodels.DynamicModel] = []
+dynamic_models.append(dynamicmodels.WhitenoiseAccelleration(sigma_a_CV, n=5))
+dynamic_models.append(dynamicmodels.ConstantTurnrate(sigma_a_CT, sigma_omega))
+ekf_filters = []
+ekf_filters.append(ekf.EKF(dynamic_models[0], measurement_model))
+ekf_filters.append(ekf.EKF(dynamic_models[1], measurement_model))
+imm_filter = imm.IMM(ekf_filters, PI)
+
+tracker = pda.PDA(imm_filter, clutter_intensity, PD, gate_size)
+
+# init_imm_pda_state = tracker.init_filter_state(init__immstate)
+
+
+NEES = np.zeros(K)
+NEESpos = np.zeros(K)
+NEESvel = np.zeros(K)
+
+tracker_update = init_imm_state
+tracker_update_list = []
+tracker_predict_list = []
+tracker_estimate_list = []
+# estimate
+for k, (Zk, x_true_k, Tsk) in enumerate(zip(Z, Xgt, Ts)):
+    tracker_predict = tracker.predict(tracker_update, Tsk)
+    tracker_update = tracker.update(Zk, tracker_predict)
+
+    # You can look at the prediction estimate as well
+    tracker_estimate = tracker.estimate(tracker_update)
+
+    NEES[k] = estats.NEES(*tracker_estimate, x_true_k, idxs=np.arange(4))
+    NEESpos[k] = estats.NEES(*tracker_estimate, x_true_k, idxs=np.arange(2))
+    NEESvel[k] = estats.NEES(*tracker_estimate, x_true_k, idxs=np.arange(2, 4))
+
+    tracker_predict_list.append(tracker_predict)
+    tracker_update_list.append(tracker_update)
+    tracker_estimate_list.append(tracker_estimate)
+
+
+x_hat = np.array([est.mean for est in tracker_estimate_list])
+prob_hat = np.array([upd.weights for upd in tracker_update_list])
+
+# calculate a performance metrics
+poserr = np.linalg.norm(x_hat[:, :2] - Xgt[:, :2], axis=0)
+velerr = np.linalg.norm(x_hat[:, 2:4] - Xgt[:, 2:4], axis=0)
+posRMSE = np.sqrt(
+    np.mean(poserr ** 2)
+)  # not true RMSE (which is over monte carlo simulations)
+velRMSE = np.sqrt(np.mean(velerr ** 2))
+# not true RMSE (which is over monte carlo simulations)
+peak_pos_deviation = poserr.max()
+peak_vel_deviation = velerr.max()
+
+
+# consistency
+confprob = 0.9
+CI2 = np.array(scipy.stats.chi2.interval(confprob, 2))
+CI4 = np.array(scipy.stats.chi2.interval(confprob, 4))
+
+confprob = confprob
+CI2K = np.array(scipy.stats.chi2.interval(confprob, 2 * K)) / K
+CI4K = np.array(scipy.stats.chi2.interval(confprob, 4 * K)) / K
+ANEESpos = np.mean(NEESpos)
+ANEESvel = np.mean(NEESvel)
+ANEES = np.mean(NEES)
+
+# %% plots
+
+ # trajectory
+fig3, axs3 = plt.subplots(1, 2, num=3, clear=True)
+axs3[0].plot(*x_hat.T[:2], label=r"$\hat x$")
+axs3[0].plot(*Xgt.T[:2], label="$x$")
+axs3[0].set_title(
+    f"RMSE(pos, vel) = ({posRMSE:.3f}, {velRMSE:.3f})\npeak_dev(pos, vel) = ({peak_pos_deviation:.3f}, {peak_vel_deviation:.3f})"
+)
+axs3[0].axis("equal")
+# probabilities
+axs3[1].plot(np.arange(K) * Ts, prob_hat)
+axs3[1].set_ylim([0, 1])
+axs3[1].set_ylabel("mode probability")
+axs3[1].set_xlabel("time")
+ 
+
+
+
+
+
+
+
+
+
+
+
+
